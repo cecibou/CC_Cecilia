5475578c88cc95134b5adca98adc12d18c24a3c8
cecibou <84025596+cecibou@users.noreply.github.com>
2021-10-19 17:17:37 -0300

    Add files via upload

---
+++ AEDS-2/tp3/TP03Q04 - Shellsort em C/listaS.c
@@ -0,0 +1,552 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <stdbool.h>
+
+typedef struct Serie
+{
+
+	char nome[100];
+	char idioma[100];
+	char formato[100];
+	char duracao[100];
+	char pais[100];
+	char emissora[100];
+	char transmicao[100];
+	int temporadas;
+	int episodios;
+} Serie;
+
+//Construtor vazio
+Serie inicializaVazio()
+{
+	Serie serie;
+	strcpy(serie.nome, "");
+	strcpy(serie.idioma, "");
+	strcpy(serie.formato, "");
+	strcpy(serie.duracao, "");
+	strcpy(serie.pais, "");
+	strcpy(serie.emissora, "");
+	strcpy(serie.transmicao, "");
+	serie.temporadas = 0;
+	serie.episodios = 0;
+
+	return serie;
+}
+
+//Construtor que recebe parâmetros
+Serie inicializa(char nome[], char idioma[], char formato[], char duracao[], char pais[], char emissora[], char transmicao[], int temporadas, int episodios)
+{
+	Serie serie;
+	strcpy(serie.nome, nome);
+	strcpy(serie.idioma, idioma);
+	strcpy(serie.formato, formato);
+	strcpy(serie.duracao, duracao);
+	strcpy(serie.pais, pais);
+	strcpy(serie.emissora, emissora);
+	strcpy(serie.transmicao, transmicao);
+	temporadas = temporadas;
+	episodios = episodios;
+
+	return serie;
+}
+
+//encapsulamento das variáveis
+void setNome(Serie *serie, char nome[])
+{
+	strcpy(serie->nome, nome);
+}
+
+void setIdioma(Serie *serie, char idioma[])
+{
+	strcpy(serie->idioma, idioma);
+}
+
+char *getIdioma(Serie serie)
+{
+	char *a;
+	strcpy(a, serie.idioma);
+	return a;
+}
+
+void setFormato(Serie *serie, char formato[])
+{
+	strcpy(serie->formato, formato);
+}
+
+char *getFormato(Serie serie)
+{
+	char *a;
+	strcpy(a, serie.formato);
+	return a;
+}
+
+void setDuracao(Serie *serie, char duracao[])
+{
+	strcpy(serie->duracao, duracao);
+}
+
+char *getDuracao(Serie serie)
+{
+	char *a;
+	strcpy(a, serie.duracao);
+	return a;
+}
+
+void setPais(Serie *serie, char pais[])
+{
+	strcpy(serie->pais, pais);
+}
+
+char *getPais(Serie serie)
+{
+	char *a;
+	strcpy(a, serie.pais);
+	return a;
+}
+
+void setEmissora(Serie *serie, char emissora[])
+{
+	strcpy(serie->emissora, emissora);
+}
+
+char *getEmissora(Serie serie)
+{
+	char *a;
+	strcpy(a, serie.emissora);
+	return a;
+}
+
+void setTransmicao(Serie *serie, char transmicao[])
+{
+	strcpy(serie->transmicao, transmicao);
+}
+
+char *getTransmicao(Serie serie)
+{
+	char *a;
+	strcpy(a, serie.transmicao);
+	return a;
+}
+
+void setTemporadas(Serie *serie, int temporadas)
+{
+	serie->temporadas = temporadas;
+}
+
+int getTemporadas(Serie serie)
+{
+	return serie.temporadas;
+}
+
+void setEpisodios(Serie *serie, int episodios)
+{
+	serie->episodios = episodios;
+}
+
+int getEpisodios(Serie serie)
+{
+	return serie.episodios;
+}
+
+int trataInt(char s[])
+{
+	int resp = 0;
+	char aux[20];
+	int j = 0;
+	for (int i = 0; i < strlen(s); i++)
+	{
+		if (s[i] == '0' || s[i] == '1' || s[i] == '2' || s[i] == '3' || s[i] == '4' || s[i] == '5' || s[i] == '6' || s[i] == '7' || s[i] == '8' || s[i] == '9')
+		{
+			aux[j] = s[i];
+			j++;
+		}
+		if (s[i] == ' ')
+		{
+			break;
+		}
+	}
+	resp = atoi(aux);
+
+	return resp;
+}
+
+char *limpaPais(char *old)
+{
+	char *newLine = (char *)malloc(sizeof(char) * strlen(old));
+	int i = 0, j = 0;
+	while (i < strlen(old))
+	{
+		if (old[i] == '<')
+		{
+			i++;
+			while (old[i] != '>')
+				i++;
+		}
+		else
+		{
+			if (old[i] == '&')
+			{
+				i += 6;
+			}
+			newLine[j] = old[i];
+			j++;
+		}
+		i++;
+	}
+	newLine[j] = '\0';
+	//printf("%s",newLine);
+	return newLine;
+}
+
+char *limpaTag(char *old)
+{
+	char *newLine = (char *)malloc(sizeof(char) * strlen(old));
+	int i = 0, j = 0;
+	while (i < strlen(old))
+	{
+		if (old[i] == '<'){
+			i++;
+			while (old[i] != '>'){
+				i++;
+			}
+
+		}
+		else
+		{
+			newLine[j] = old[i];
+			j++;
+		}
+		i++;
+	}
+	newLine[j] = '\0';
+	//printf("%s",newLine);
+	return newLine;
+}
+
+char* tiraEspaco(char *old){
+
+ char *newLine = (char *)malloc(sizeof(char) * strlen(old));
+    int c = 0, d = 0;
+	if(old[0] == ' '){
+        d = 1;
+    }
+    while(d < strlen(old)){
+        newLine[c] = old[d];
+        c++;
+        d++;
+    }
+ 
+    newLine[c] = '\0';
+	return newLine;
+ }
+
+ char* tiraEspaco2(char *old){
+
+ char *newLine = (char *)malloc(sizeof(char) * strlen(old));
+    int c = 0, d = 0;
+	if(old[0] == ' '){
+        d = 1;
+    }
+    while(d < strlen(old)){
+        newLine[c] = old[d];
+        c++;
+        d++;
+    }
+ 
+    newLine[c] = '\0';
+	return newLine;
+ }
+
+ char* tiraEspacoGrey(char *old){
+	 char *newLine = (char *)malloc(sizeof(char) * strlen(old));
+	 int i = 0;
+	 //Estados Unidos ;
+	 for(i = 0; i <= 14; i++){
+		 newLine[i] = old[i];
+	 }
+	 newLine[i] = '\0';
+	return newLine;
+ }
+
+ //método para tratar o nome do arquivo e retornar o nome da série
+char* pegaNome(char *nome){
+	int tam = strlen(nome);
+	int pos = 0;
+	char *nome2 = (char *)malloc(sizeof(char) * (tam));
+	for(int i = 0; i < tam; i++){
+		if(nome[i]  == '_'){ //caso o caracter na posição i seja igual ao '_' a variável resp recebe um espaço em branco
+			nome2[pos]  = ' ';
+			nome2[pos+1] = '\0';
+			pos = pos + 1;
+		} else { //caso não tenha espaço em branco o caracter é concatenado à string resp
+			if(nome[i] == '.'){
+				i = tam;
+			}else{
+				nome2[pos] = nome[i];
+				nome2[pos + 1] = '\0';
+				pos++;
+			}
+		}
+		}
+	return nome2;
+}
+
+
+
+//funcao que le no meu pub.in
+void ler(Serie *serie, char *nome)
+{
+	//FILE *fp = fopen(nome, "r");
+	char filePath[200];
+	sprintf(filePath, "/tmp/series/%s", nome);
+	FILE *fp = fopen(filePath, "r");
+
+	char buf[4000];
+	fgets(buf, 4000, fp);
+    
+	while(strstr(buf, "infobox_v2") == NULL){
+		fgets(buf, 3000, fp);
+	}
+	fgets(buf,3000,fp);
+	fgets(buf,3000,fp);
+	strcpy(serie->nome,pegaNome(nome));
+	//strcpy(serie->nome,limpaTag(buf));
+	//serie->nome[strcspn(serie->nome, "\n")] = ' ';
+    //printf("%s", searchName(nome));
+
+	while (strstr(buf, ">Formato<") == NULL)
+		fgets(buf, 3000, fp);
+	fgets(buf, 3000, fp);
+	strcpy(serie->formato, (limpaTag(buf)));
+	//strcpy(serie->formato, tiraEspaco(buf));
+	serie->formato[strcspn(serie->formato, "\n")] = ' ';
+
+	while (strstr(buf, ">Dura") == NULL)
+		fgets(buf, 3000, fp);
+	fgets(buf, 3000, fp);
+	strcpy(serie->duracao, (limpaTag(buf)));
+	serie->duracao[strcspn(serie->duracao, "\n")] = ' ';
+
+	while (strstr(buf, "s de origem<") == NULL)
+		fgets(buf, 3000, fp);
+	fgets(buf, 3000, fp);
+	strcpy(serie->pais, tiraEspaco(limpaPais(limpaTag(buf))));
+	serie->pais[strcspn(serie->pais, "\n")] = ' ';
+	if (strstr(serie-> nome, "Grey's Anatomy") != NULL){
+		strcpy(serie->pais, tiraEspacoGrey(serie->pais));
+	}
+	while (strstr(buf, ">Idioma original<") == NULL)
+		fgets(buf, 3000, fp);
+	fgets(buf, 3000, fp);
+	strcpy(serie->idioma, tiraEspaco(limpaTag(buf)));
+	serie->idioma[strcspn(serie->idioma, "\n")] = ' ';
+
+	while (strstr(buf, ">Emissora de televis") == NULL)
+		fgets(buf, 3000, fp);
+	fgets(buf, 3000, fp);
+	strcpy(serie->emissora, (limpaTag(buf)));
+	serie->emissora[strcspn(serie->emissora, "\n")] = ' ';
+
+	while (strstr(buf, ">Transmiss") == NULL)
+		fgets(buf, 3000, fp);
+	fgets(buf, 3000, fp);
+	strcpy(serie->transmicao, (limpaPais(limpaTag(buf))));
+	serie->transmicao[strcspn(serie->transmicao, "\n")] = ' ';
+
+	while (strstr(buf, " de temporadas<") == NULL)
+		fgets(buf, 3000, fp);
+	fgets(buf, 3000, fp);
+	serie->temporadas = trataInt(limpaTag(buf));
+	//serie->temporadas[strcspn(serie->temporadas, "\n")] = ' ';
+
+	while (strstr(buf, " de epis") == NULL)
+		fgets(buf, 3000, fp);
+	fgets(buf, 3000, fp);
+	serie->episodios = trataInt(limpaTag(buf));
+
+	fclose(fp);
+}
+
+//funcao que imprimi de acordo com o meu pub.out
+void imprimir(Serie serie)
+{
+	printf("%s %s%s%s%s%s%s%d %d\n", serie.nome, serie.formato, serie.duracao, serie.pais, serie.idioma, serie.emissora, serie.transmicao, serie.temporadas, serie.episodios);
+}
+
+#define MAXTAM 100
+
+
+Serie series[MAXTAM];
+int n;
+void start(){ 
+	n = 0;
+}
+
+void inserirInicio(Serie x) {
+	if (n >= MAXTAM)
+		exit(1);
+	//levar elementos para o fim do series
+	for (int i = n; i > 0; i--){
+		series[i] = series[i-1];
+	}
+	series[0] = x;
+	n++;
+}
+
+void inserirFim(Serie x) {
+	if (n >= MAXTAM)
+		exit(1);
+	series[n] = x;
+	n++;
+}
+
+void inserir(Serie x, int pos) {
+	if (n >= MAXTAM || pos < 0 || pos > n)
+		exit(1);
+	//levar elementos para o fim do series
+	for (int i = n; i > pos; i--){
+		series[i] = series[i-1];
+	}
+	series[pos] = x;
+	n++;
+}
+
+Serie removerInicio() {
+	if (n == 0)
+		exit(1);
+	Serie resp = series[0];
+	n--;
+	for (int i = 0; i < n; i++){
+		series[i] = series[i+1];
+	}
+	return resp;
+}
+
+Serie removerFim() {
+	if (n == 0)
+		exit(1);
+	return series[--n];
+}
+
+Serie remover(int pos) {
+	if (n == 0 || pos < 0 || pos >= n)
+		exit(1);
+	Serie resp = series[pos];
+	n--;
+	for (int i = pos; i < n; i++){
+		series[i] = series[i+1];
+	}
+	//n--;
+	return resp;
+}
+
+void mostrar (){
+
+	for (int i = 0; i < n; i++){
+		imprimir(series[i]);
+	}
+
+}
+
+void swap(int x, int y){
+        Serie tmp = series[x];
+        series[x] = series[y];
+        series[y] = tmp;
+    }
+    
+void selecao(int i){
+
+    if (i < (n - 1)){
+        int menor = i;
+        for (int j = (i + 1); j < n; j++){
+            if (strcmp(series[menor].pais, series[j].pais)>0){
+                menor = j;
+            }else if(strcmp(series[menor].pais,series[j].pais) == 0){
+                if(strcmp(series[menor].nome,series[j].nome)>0){
+                    menor = j;
+                }
+            }
+        }
+        swap(menor, i);
+		selecao(i+1);
+        }
+        ///mostrar();
+}
+
+void insercaoPorCor(int n, int cor, int h){
+    for (int i = (h + cor); i < n; i+=h) {
+        Serie tmp = series[i];
+        int j = i - h;
+        while ((j >= 0) && ((strcmp(series[j].idioma, tmp.idioma)) > 0 || (strcmp(series[j].idioma, tmp.idioma) == 0 && (strcmp(series[j].nome, tmp.nome) > 0)))) {
+            series[j + h] = series[j];
+            j-=h;
+        }
+        series[j + h] = tmp;
+    }
+}
+
+void shellsort(int n) {
+    int h = 1;
+
+    do { h = (h * 3) + 1; } while (h < n);
+
+    do {
+        h /= 3;
+        for(int cor = 0; cor < h; cor++){
+            insercaoPorCor(n, cor, h);
+        }
+    } while (h != 1);
+}
+
+char* pegaString(char* old, int i){
+	char *newLine = (char *)malloc(sizeof(char) * strlen(old));
+	int j = 0;
+	while (i < strlen(old))
+	{
+		newLine[j] = old[i];
+
+		i++;
+		j++;
+	}
+	newLine[j] = '\0';
+	//printf("%s",newLine);
+	return newLine;
+}
+
+int pegaNum(char* old){
+	char newLine[2];
+	newLine[0] = old[3];
+	newLine[1] = old[4];
+	int resp = atoi(newLine);
+	return resp;
+}
+
+//Parada no FIM
+bool isFim(char s[]){
+
+	return (s[0] == 'F' && s[1] == 'I' && s[2] == 'M');
+}
+
+int main (){
+	char entrada[200][200];
+	int numEntrada = 0;
+	int n;
+
+	// Leitura da entrada padrao
+	do
+	{
+		scanf(" %[^\n]s", entrada[numEntrada]);
+		if(strcmp(entrada[numEntrada], "FIM") != 0){
+			Serie s;
+			ler(&s , entrada[numEntrada]);
+			inserirFim(s);
+		}
+	} while (!isFim(entrada[numEntrada++]));
+	numEntrada--; // Desconsiderar ultima linha contendo a palavra FIM
+
+    shellsort(numEntrada);
+	mostrar();
+	
+	return 0;
+}
---
+++ AEDS-2/tp3/TP03Q09 - Mergesort em Java/ListaM.java
@@ -0,0 +1,425 @@
+import java.io.*;
+
+class SerieV {
+    private String nome;
+    private String idioma;
+    private String formato;
+    private String duracao;
+    private String pais;
+    private String emissora;
+    private String transmicao;
+    private int temporadas;
+    private int episodios;
+
+    // Construtor vazio
+    public SerieV() {
+        this.idioma = "";
+        this.nome = "";
+        this.duracao = "";
+        this.formato = "";
+        this.pais = "";
+        this.emissora = "";
+        this.transmicao = "";
+        this.temporadas = 0;
+        this.episodios = 0;
+    }
+
+    // Construtor que recebe parâmetros
+    SerieV(String nome, String idioma, String duracao, String formato, String pais, String emissora, String transmicao,
+            int temporadas, int episodios) {
+        this.nome = nome;
+        this.idioma = idioma;
+        this.duracao = duracao;
+        this.formato = formato;
+        this.pais = pais;
+        this.emissora = emissora;
+        this.transmicao = transmicao;
+        this.temporadas = temporadas;
+        this.episodios = episodios;
+    }
+
+    // encapsulamento das variáveis
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getNome() {
+        return this.nome;
+    }
+
+    public void setIdioma(String idioma) {
+        this.idioma = idioma;
+    }
+
+    public String getIdioma() {
+        return this.idioma;
+    }
+
+    public void setFormato(String formato) {
+        this.formato = formato;
+    }
+
+    public String getFormato() {
+        return this.formato;
+    }
+
+    public void setDuracao(String duracao) {
+        this.duracao = duracao;
+    }
+
+    public String getDuracao() {
+        return this.duracao;
+    }
+
+    public void setPais(String pais) {
+        this.pais = pais;
+    }
+
+    public String getPais() {
+        return this.pais;
+    }
+
+    public void setEmissora(String emissora) {
+        this.emissora = emissora;
+    }
+
+    public String getEmissora() {
+        return this.emissora;
+    }
+
+    public void setTransmicao(String transmicao) {
+        this.transmicao = transmicao;
+    }
+
+    public String getTransmicao() {
+        return this.transmicao;
+    }
+
+    public void setTemporadas(int temporadas) {
+        this.temporadas = temporadas;
+    }
+
+    public int getTemporadas() {
+        return this.temporadas;
+    }
+
+    public void setEpisodios(int episodios) {
+        this.episodios = episodios;
+    }
+
+    public int getEpisodios() {
+        return this.episodios;
+    }
+
+    public void clone(SerieV x) {
+        this.nome = x.getNome();
+        this.idioma = x.getIdioma();
+        this.duracao = x.getDuracao();
+        this.formato = x.getFormato();
+        this.pais = x.getPais();
+        this.emissora = x.getEmissora();
+        this.transmicao = x.getTransmicao();
+        this.temporadas = x.getTemporadas();
+        this.episodios = x.getEpisodios();
+    }
+
+    public static String limpaTag(String s) {
+        String resp = "";
+        boolean dentroTag = false;
+        for (int i = 0; i < s.length(); i++) {
+            if (s.charAt(i) == '<') {
+                // resp += s.charAt(i);
+                dentroTag = false;
+            } else if (s.charAt(i) == '>') {
+                // resp += s.charAt(i);
+                dentroTag = true;
+            } else if (dentroTag == true) {
+                resp += s.charAt(i);
+            }
+        }
+        return resp;
+    }
+
+    public static int trataInt(String s) {
+        int resp = 0;
+        String aux = "";
+        // MyIO.print(s);
+        for (int i = 0; i < s.length(); i++) {
+            if (s.charAt(i) == '0' || s.charAt(i) == '1' || s.charAt(i) == '2' || s.charAt(i) == '3'
+                    || s.charAt(i) == '4' || s.charAt(i) == '5' || s.charAt(i) == '6' || s.charAt(i) == '7'
+                    || s.charAt(i) == '8' || s.charAt(i) == '9') {
+                aux += s.charAt(i);
+            }
+            if (s.charAt(i) == ' ') {
+                break;
+            }
+        }
+        resp = Integer.parseInt(aux);
+
+        return resp;
+    }
+
+    public static String tratapais(String s) {
+        String resp = "";
+        for (int i = 0; i < s.length(); i++) {
+            if (s.charAt(i) == '&') {
+                i += 6;
+            }
+            resp += s.charAt(i);
+        }
+
+        return resp;
+    }
+
+    public static String tratatransmissao(String s) {
+        String resp = "";
+        for (int i = 0; i < s.length(); i++) {
+            if (s.charAt(i) == '&') {
+                i += 6;
+            }
+            resp += s.charAt(i);
+        }
+
+        return resp;
+    }
+
+
+    //método para tratar o nome do arquivo e retornar o nome da série
+    public String searchName(String fileName){
+        String resp = "";
+        for(int i = 0; i < fileName.length(); i++){
+            if(fileName.charAt(i)  == '_'){ //caso o caracter na posição i seja igual ao '_' a variável resp recebe um espaço em branco
+                resp += ' ';
+            } else { //caso não tenha espaço em branco o caracter é concatenado à string resp
+                resp += fileName.charAt(i);
+            }
+        }
+        return resp.substring(0, resp.length()-5); //retorno da substring resp retirando os 5 últimos caracteres relacionados à extensão do arquivo
+    }
+
+    // funcao que le no meu pub.in
+    public void ler(String nome, String endereco) throws Exception {
+        InputStreamReader irs = new InputStreamReader(new FileInputStream(endereco));
+
+        BufferedReader br = new BufferedReader(irs);
+        while (!br.readLine().contains("<table class=\"infobox_v2\""))
+            ;
+        br.readLine();
+
+        this.nome = searchName(nome);
+
+        while (!br.readLine().contains(">Formato<"))
+            ;
+        setFormato(limpaTag(br.readLine()));
+        // MyIO.println(this.formato);
+        while (!br.readLine().contains(">Dura"))
+            ;
+        setDuracao(limpaTag(br.readLine()).trim());
+        // MyIO.println(this.duracao);
+        while (!br.readLine().contains("s de origem<"))
+            ;
+        setPais(tratapais(limpaTag(br.readLine())).trim());
+        // MyIO.println(this.pais);
+        while (!br.readLine().contains(">Idioma"))
+            ;
+        setIdioma(limpaTag(br.readLine()).trim());
+        // MyIO.println(this.idioma);
+        while (!br.readLine().contains(">Emissora de televis"))
+            ;
+        setEmissora(limpaTag(br.readLine()).trim());
+        // MyIO.println(this.emissora);
+        while (!br.readLine().contains(">Transmiss"))
+            ;
+        setTransmicao(tratatransmissao(limpaTag(br.readLine())).trim());
+        // MyIO.println(this.transmicao);
+        while (!br.readLine().contains(" de temporadas<"))
+            ;
+        setTemporadas(trataInt(limpaTag(br.readLine())));
+
+        while (!br.readLine().contains(" de epis"))
+            ;
+        setEpisodios(trataInt(limpaTag(br.readLine())));
+        br.close();
+    }
+
+    // funcao que imprimi de acordo com o meu pub.out
+    public String imprimir() {
+        return this.nome + " " + this.formato + " " + this.duracao + " " + this.pais + " " + this.idioma + " "
+                + this.emissora + " " + this.transmicao + " " + this.temporadas + " " + this.episodios;
+
+    }
+
+}
+
+public class ListaM {
+    //iníco a implementação da lista por meio de arranjos
+    private SerieV series[];
+    private int n;
+    public ListaM (int maxTam) { // A constante maxTam define o tamanho maximo permitido na lista
+        this.series = new SerieV[maxTam];
+        this.n = 0;
+    }
+
+ 
+
+    //insere no inicio
+    void inserirInicio(SerieV x) throws Exception {
+        if (n >= series.length)
+        throw new Exception("Erro!");
+        //levar elementos para o fim do series
+        for (int i = n; i > 0; i--){
+        series[i] = series[i-1];
+        }
+        series[0] = x;
+        n++;
+        }
+
+    void inserirFim(SerieV x) throws Exception {
+        if (n >= series.length)
+        throw new Exception("Erro!");
+        series[n] = x;
+        n++;
+    }
+    
+    void inserir(SerieV x, int pos) throws Exception {
+        if (n >= series.length || pos < 0 || pos > n)
+        throw new Exception("Erro!");
+        //levar elementos para o fim do series
+        for (int i = n; i > pos; i--){
+        series[i] = series[i-1];
+        }
+        series[pos] = x;
+        n++;
+        }
+
+    SerieV removerInicio() throws Exception {
+        if (n == 0)
+        throw new Exception("Erro!");
+        SerieV resp = series[0];
+        n--;
+        for (int i = 0; i < n; i++){
+        series[i] = series[i+1];
+        }
+        return resp;
+        }
+
+    SerieV removerFim() throws Exception {
+        if (n == 0)
+        throw new Exception("Erro!");
+        return series[--n];
+    }
+
+    SerieV remover(int pos) throws Exception {
+        if (n == 0 || pos < 0 || pos >= n)
+        throw new Exception("Erro!");
+        SerieV resp = series[pos];
+        n--;
+        for (int i = pos; i < n; i++){
+        series[i] = series[i+1];
+        }
+        return resp;
+        }
+        
+    void mostrar (){
+        
+        for (int i = 0; i < n; i++){
+            MyIO.println(series[i].imprimir());
+        }
+        
+    } 
+
+    // void swap(int x, int y){
+    //     SerieV tmp = series[x];
+    //     series[x] = series[y];
+    //     series[y] = tmp;
+    // }
+    
+    // void selecao(){
+    //     for (int i = 0; i < (n - 1); i++) {
+    //         int menor = i;
+    //         for (int j = (i + 1); j < n; j++){
+    //            if (series[menor].getPais().compareTo(series[j].getPais()) > 0 ){
+    //               menor = j;
+    //            }else if(series[menor].getPais().equals(series[j].getPais())){
+    //                if(series[menor].getNome().compareTo(series[j].getNome()) > 0 ){
+    //                    menor = j;
+    //                }
+    //            }
+    //         }
+    //         swap(menor, i);
+    //      }
+    //      mostrar();
+    // }
+
+    public void intercalar(int esq, int meio, int dir){
+        int n1, n2, i, j, k;
+  
+        //Definir tamanho dos dois subseriess
+        n1 = meio-esq+1;
+        n2 = dir - meio;
+  
+        SerieV[] a1 = new SerieV[n1+1];
+        SerieV[] a2 = new SerieV[n2+1];
+  
+        //Inicializar primeiro subseries
+        for(i = 0; i < n1; i++){
+           a1[i] = series[esq+i];
+        }
+  
+        //Inicializar segundo subseries
+        for(j = 0; j < n2; j++){
+           a2[j] = series[meio+j+1];
+        }
+  
+        //Sentinela no final dos dois seriess
+        a1[i].setEpisodios(0x7FFFFFFF); 
+        a2[j].setEpisodios(0x7FFFFFFF);
+  
+        //Intercalacao propriamente dita
+        for(i = j = 0, k = esq; k <= dir; k++){
+           series[k] = (a1[i].getEpisodios() <= a2[j].getEpisodios()) ? a1[i++] : a2[j++];
+        }
+     }
+
+    //  ((a1[i].getEpisodios() <= a2[j].getEpisodios()) || ((series[i].getEpisodios() == (series[j].getEpisodios())) && (series[j].getNome().compareTo(series[j].getNome()) > 0)))
+
+    private void mergesort(int esq, int dir) {
+        if (esq < dir){
+           int meio = (esq + dir) / 2;
+           mergesort(esq, meio);
+           mergesort(meio + 1, dir);
+           intercalar(esq, meio, dir);
+        }
+     }
+
+
+
+    public static boolean isFim(String s) {
+        return (s.length() == 3 && s.charAt(0) == 'F' && s.charAt(1) == 'I' && s.charAt(2) == 'M');
+    }
+
+    public static void main(String[] args) throws Exception {
+        MyIO.setCharset("UTF-8");
+        String[] entrada = new String[1000];
+        int numEntrada = 0;
+        ListaM lista = new ListaM(70);
+        SerieV s;
+        //String url = "series/";
+        String url = "/tmp/series/";
+
+        // Leitura da entrada padrao
+        do {
+            entrada[numEntrada] = MyIO.readLine();
+            String endereco = url + entrada[numEntrada];
+            s = new SerieV();
+            if (isFim(entrada[numEntrada]) == false) {
+                s.ler(entrada[numEntrada], endereco);
+                lista.inserirFim(s);
+            }
+        } while (isFim(entrada[numEntrada++]) == false);
+        numEntrada--; // Desconsiderar ultima linha contendo a palavra FIM
+
+
+        lista.mergesort(0, numEntrada-1);
+    }
+}
